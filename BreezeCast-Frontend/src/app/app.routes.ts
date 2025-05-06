fe340fd1cd63d4ce5b64ed82bf4c3ccfe7918c80
rhysqi <rhysqi.akbar@gmail.com>
2025-05-06 08:29:29 +0700

    Remove: Breeze Frontend
    submit: rhysqi

--- BreezeCast-Frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
-
-export const routes: Routes = [
-	{
-		path: '',
-		component: MainLayoutComponent
-	}
-];
